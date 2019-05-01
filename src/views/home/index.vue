4978fadf0ed8eb45e5da3d9ee608298c819c794e
yajizuozuo <yajizuozuo@163.com>
2019-05-01 22:36:54 +0800

    项目初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <button @click="gopage">登录</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    gopage() {
+      this.$router.push({
+        path: '/login'
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
