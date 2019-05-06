03cd0b8ab2517756108de6bc9efd15452d2770e0
yajizuozuo <yajizuozuo@163.com>
2019-05-07 00:54:34 +0800

    no message

--- src/views/work-order/index.vue
+++ src/views/work-order/index.vue
@@ -13,7 +13,7 @@
         </div>
         <el-steps :active="2" align-center>
           <el-step title="步骤1" description="这是一段很长很长很长的描述性文字" />
-          <el-step title="步骤2" description="这是一段很长很长很长的描述性文字" icon="iconfont el-icon-refresh" />
+          <el-step title="步骤2" description="这是一段很长很长很长的描述性文字" icon="el-icon-refresh" />
           <el-step title="步骤3" description="这是一段很长很长很长的描述性文字" />
           <el-step title="步骤4" description="这是一段很长很长很长的描述性文字" />
         </el-steps>
